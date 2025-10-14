59713a8fa15674a615ced28c4a0a4881df4a6bc8
S Mendoza <samuelmendozal06@gmail.com>
2025-10-13 22:59:34 -0500

    Creacion clase Ingresos

--- src/models/IngresosEmprendedor.java
+++ src/models/IngresosEmprendedor.java
@@ -1,13 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package models;
 
-/**
- *
- * @author samuelmendoza
- */
+package models;
+import java.time.LocalDate;
+
 public class IngresosEmprendedor {
+    
+    private double MontoIngreso;
+    private LocalDate fecha;
+    private String Descripcion;
+    private String MetodoPago;
+
+    public IngresosEmprendedor(double MontoIngreso, LocalDate fecha, String Descripcion, String MetodoPago) {
+        this.MontoIngreso = MontoIngreso;
+        this.fecha = fecha;
+        this.Descripcion = Descripcion;
+        this.MetodoPago = MetodoPago;
+    }
+
+    public double getMontoIngreso() {
+        return MontoIngreso;
+    }
+
+    public void setMontoIngreso(double MontoIngreso) {
+        this.MontoIngreso = MontoIngreso;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    public String getMetodoPago() {
+        return MetodoPago;
+    }
+
+    public void setMetodoPago(String MetodoPago) {
+        this.MetodoPago = MetodoPago;
+    }
+
+    @Override
+    public String toString() {
+        return "IngresosEmprendedor{" + "MontoIngreso=" + MontoIngreso + ", fecha=" + fecha + ", Descripcion=" + Descripcion + ", MetodoPago=" + MetodoPago + '}';
+    }
+    
+    
     
 }
